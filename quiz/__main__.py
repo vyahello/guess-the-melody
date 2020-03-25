78ff5b2277101dcfbf15d0a946e0a0085d4552dc
Volodymyr Yahello <vyahello@gmail.com>
2020-03-25 22:47:41 +0200

    Ignore '--run' CLI flag

--- quiz/__main__.py
+++ quiz/__main__.py
@@ -5,16 +5,12 @@ from quiz.handler import bot
 
 
 @click.command()
-@click.option("--run", "-r", default=False, show_default=True, is_flag=True, help="Allows to run telegram bot app")
 @click.option("--key", "-k", help="Telegram bot api key")
-def main(run: bool, key: str, **kwargs: Any) -> None:
-    """Runs telegram bot app."""
-    if run:
-        bprint("Running quiz bot (press ctrl+c to escape)")
-        bot.polling(none_stop=True, **kwargs)
-    else:
-        bprint("Nothing to run.")
+def __main(run: bool, key: str, **kwargs: Any) -> None:
+    """The program allows to run telegram bot application."""
+    bprint("Running quiz bot (press ctrl+c to escape)")
+    bot.polling(none_stop=True, **kwargs)
 
 
 if __name__ == "__main__":
-    main()
+    __main()
